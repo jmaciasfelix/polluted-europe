4c5bd894a90aba87c12ac10189b300ce7f26c71f
Jesús Macías Félix <jmacias@atsistemas.com>
2021-02-03 20:37:13 +0100

    Create a radial background

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 //pages
-import Home from "./pages/Home";
+import { Home } from "./pages/Home";
+//components
+import { Layout } from "./components/Layout";
 
-const App = () => <Home />;
+const App = () => (
+  <Layout>
+    <Home />
+  </Layout>
+);
 
 export default App;
--- front/src/pages/Home/index.js
+++ front/src/pages/Home/index.js
@@ -3,9 +3,7 @@ import PropTypes from "prop-types";
 //hooks
 import { usePollutedCities } from "../../hooks/usePollutedCities";
 
-const Home = () => {
+export const Home = () => {
   const [pollutedCities] = usePollutedCities();
   return <h1>Polluted cities 🌍</h1>;
 };
-
-export default Home;
