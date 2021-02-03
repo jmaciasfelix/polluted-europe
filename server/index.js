6a1e6260c131c61a629f36b63175769971f6b52a
Jesús Macías Félix <jmacias@atsistemas.com>
2021-02-03 19:51:12 +0100

    A WebSocket connection is established between the front and the back. Install React router, wouter and styled components.

--- server/index.js
+++ server/index.js
@@ -1,32 +1,53 @@
+const fs = require("fs");
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Connection stablished");
 });
 
+const sendRandomJson = (socket) => {
+  const min = 1;
+  const max = 4;
+  const randomNumber = Math.trunc(Math.random() * (max - min) + min);
+  fs.readFile(
+    `./data/citiesPollutionEurope_${randomNumber}.json`,
+    (err, data) => {
+      if (err) throw err;
+      const pollutionCities = JSON.parse(data);
+      socket.emit("FromAPI", pollutionCities);
+    }
+  );
+};
+
 let interval;
+var timeInterval = 300000;
 
 io.on("connection", (socket) => {
   console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  sendRandomJson(socket);
+
+  interval && clearInterval(interval);
+  interval = setInterval(() => sendRandomJson(socket), timeInterval);
+
   socket.on("disconnect", () => {
     console.log("Client disconnected");
     clearInterval(interval);
   });
 });
 
-const getApiAndEmit = (socket) => {
-  const response = new Date();
-  socket.emit("FromAPI", response);
-};
+server.listen(8080, () => {
+  timeInterval = process.argv[2] ? process.argv[2] : timeInterval;
 
-server.listen(8080, () => console.log(`Listening on port 8080`));
+  console.log(`Listening on port 8080`);
+  console.log(`The server sends new information every ${timeInterval} ms`);
+});
