bc7423b3b09439e78ca5bc62695dec87fe9ae414
Jesús Macías Félix <jmacias@atsistemas.com>
2021-02-04 08:23:53 +0100

    Create a table using ReactTable. This table have the sort functionality

--- front/src/components/Layout/index.js
+++ front/src/components/Layout/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const Container = styled.div`
   padding-right: 15px;
--- front/src/pages/Home/index.js
+++ front/src/pages/Home/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+//components
+import { Table } from "../../components/Table";
 //hooks
 import { usePollutedCities } from "../../hooks/usePollutedCities";
 
 export const Home = () => {
   const [pollutedCities] = usePollutedCities();
-  return <h1>Polluted cities 🌍</h1>;
+  return (
+    <div>
+      <h1>Polluted cities 🌍</h1>
+      {pollutedCities && <Table data={pollutedCities} />}
+    </div>
+  );
 };
