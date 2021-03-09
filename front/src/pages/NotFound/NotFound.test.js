534f7eb93449a094b27ac54bcfe1b46887e9cbd8
Jesus <maciasfelixj@gmail.com>
2021-03-09 17:07:38 +0100

    Create tests for NotFound

--- front/src/pages/NotFound/NotFound.test.js
+++ front/src/pages/NotFound/NotFound.test.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { NotFound } from "./index";
 
-describe("NotFound test", () => {
-  test("render Title", () => {
-    const { getByText } = render(<NotFound />);
-    const textElement = getByText(/404 Not Found .../i);
-    expect(textElement).toBeInTheDocument();
+describe("test suite NotFound component", () => {
+  test("render NotFound component", () => {
+    render(<NotFound />);
+    expect(screen.getByText(/404 Not Found .../i)).toBeInTheDocument();
+  });
+  test("check that img is not empty", () => {
+    render(<NotFound />);
+    expect(screen.getByRole("img").src).toBeDefined()
   });
 });
